62e99836177bfdffdc1316352d35a592634315eb
Kamil Kowalski <kamil@olicom.pl>
2021-10-12 23:45:34 +0200

    Methods

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods: {
+    counterUp() {
+      this.counter++;
+    },
+    counterDown() {
+      this.counter--;
+    }
+  }
 });
 
 app.mount('#events');
